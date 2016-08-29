306597b0dba74cca86fad98c8570b2844d2b676c
Bastian Eicher <bastian@eicher.net>
2016-08-29 11:05:12 +0200

    Tweaked build script

--- build.cmd
+++ build.cmd
@@ -12,14 +12,14 @@ if not defined VS140COMNTOOLS (
 )
 call "%VS140COMNTOOLS%vsvars32.bat"
 
-::Compile Visual Studio solution
+echo Compiling Visual Studio solution...
 nuget restore WebApi.BasicAuth.sln
-msbuild WebApi.BasicAuth.sln /nologo /t:Rebuild /p:Configuration=%BuildConfiguration%
+msbuild WebApi.BasicAuth.sln /nologo /v:q /m /t:Rebuild /p:Configuration=%BuildConfiguration%
 if errorlevel 1 pause
 
-::Create NuGet packages
+echo Creating NuGet packages...
 mkdir build\%BuildConfiguration%\Packages
-nuget pack WebApi.BasicAuth\WebApi.BasicAuth.csproj -Properties Configuration=%BuildConfiguration% -Symbols -OutputDirectory build\%BuildConfiguration%\Packages
+nuget pack WebApi.BasicAuth\WebApi.BasicAuth.csproj -Verbosity quiet -Properties Configuration=%BuildConfiguration% -Symbols -OutputDirectory build\%BuildConfiguration%\Packages
 if errorlevel 1 pause
 
 popd
